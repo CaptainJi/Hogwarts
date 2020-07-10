39cef7e5dfd64c8d837dec79431548f1f6a8241d
CaptainJi <jiqing19861123@163.com>
2020-07-10 14:53:12 +0800

    更新写法,生成更多测试用例 更正yaml文件中的数据写法,以更改读取yaml文件出现的数据类型错误

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -5,40 +5,41 @@ import sys
 sys.path.append('..')
 from pythoncode.caic import Calculator
 
-
+# 定义测试类
 class TestCalc():
     cal = Calculator()
 
-    @pytest.mark.parametrize('a', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'bit_int',
+    # 参数化
+    @pytest.mark.parametrize('a', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'big_int',
                                                                           'str'])
-    @pytest.mark.parametrize('b', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'bit_int',
+    @pytest.mark.parametrize('b', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'big_int',
                                                                           'str'])
     def test_add(self, a, b):
         result = a + b
         print(result)
         assert result == self.cal.add(a, b)
 
-    @pytest.mark.parametrize('a', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'bit_int',
+    @pytest.mark.parametrize('a', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'big_int',
                                                                           'str'])
-    @pytest.mark.parametrize('b', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'bit_int',
+    @pytest.mark.parametrize('b', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'big_int',
                                                                           'str'])
     def test_sub(self, a, b):
         result = a - b
         print(result)
         assert result == self.cal.sub(a, b)
 
-    @pytest.mark.parametrize('a', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'bit_int',
+    @pytest.mark.parametrize('a', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'big_int',
                                                                           'str'])
-    @pytest.mark.parametrize('b', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'bit_int',
+    @pytest.mark.parametrize('b', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'big_int',
                                                                           'str'])
     def test_mul(self, a, b):
         result = a * b
         print(result)
         assert result == self.cal.mul(a, b)
 
-    @pytest.mark.parametrize('a', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'bit_int',
+    @pytest.mark.parametrize('a', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'big_int',
                                                                           'str'])
-    @pytest.mark.parametrize('b', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'bit_int',
+    @pytest.mark.parametrize('b', yaml.safe_load(open('data.yaml')), ids=['int', 'minus', 'zero', 'float', 'big_int',
                                                                           'str'])
     def test_div(self, a, b):
         result = a / b
